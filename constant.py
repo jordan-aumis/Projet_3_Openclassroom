b3ffdcdd5aff5e22cf7331984a1864cdfe63aad1
Jordan Aumis <jordan0aumis@gmail.com>
2019-09-10 19:24:36 +0200

    oraganisation du code

--- constant.py
+++ constant.py
@@ -1,11 +1,24 @@
+import pygame
 
-GAME_DISPLAY = pygame.display.set_mode((425, 250))
+GAME_DISPLAY = pygame.display.set_mode((425, 475))
+# seringe_img = pygame.image.load(os.path.join('ressource', 'seringue.png'))
+# needle_img = pygame.image.load(os.path.join('ressource', 'aiguille.png'))
+# plastic_tube_img = pygame.image.load(os.path.join('ressource', 'tube_plastique.png'))
+# ether_img = pygame.image.load(os.path.join('ressource', 'ether.png'))
+# mc_gyver_img = pygame.image.load(os.path.join('ressource', 'MacGyver.png'))
+# floor_img = pygame.image.load(os.path.join('ressource', 'floor.png'))
+# background_color = pygame.Color(0, 0, 0)
+MAZE = []
+grid = open("labyrinth.txt", "r")
+for line in grid:
+    a = line.replace("/n", "")
+    raw = a.split()
+    for case in raw:
+        col = case.split(",")
+    MAZE.append(col)
+grid.close()
 
-seringe_img = pygame.image.load(os.path.join('ressource', 'seringue.png'))
-needle_img = pygame.image.load(os.path.join('ressource', 'aiguille.png'))
-plastic_tube_img = pygame.image.load(os.path.join('ressource', 'tube_plastique.png'))
-ether_img = pygame.image.load(os.path.join('ressource', 'ether.png'))
-mc_gyver_img = pygame.image.load(os.path.join('ressource', 'MacGyver.png'))
-floor_img = pygame.image.load(os.path.join('ressource', 'floor.png'))
-background_color = pygame.Color(0, 0, 0)
+YELLOW = pygame.Color(255, 255, 0)
+GREEN = pygame.Color(75, 181, 67)
+RED = pygame.Color(255, 0, 0)
 SCREENABLE = 25
