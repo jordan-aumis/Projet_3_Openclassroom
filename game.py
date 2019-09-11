96caf1980687326d8d4159f127ec4925b1dfadd5
Jordan Aumis <jordan0aumis@gmail.com>
2019-09-11 20:42:17 +0200

    organisation et paufinage poo

--- game.py
+++ game.py
@@ -2,55 +2,69 @@
 first i created a 2d list first list is the raw and second the col.
 Then i choose letter and numbers to set the labyrinth.
 """
-import constant
 import os
 import random
-import pygame
 import time
+import pygame
+import constant
 
-class Objet:
-    TAKEN = False
+class Item:
+    """set of Items and method"""
+    taken = False
 
     def __init__(self, pos_x=0, pos_y=0):
+        """Create the item set his position to 0 by default"""
         self.pos_x = pos_x
         self.pos_y = pos_y
 
+    def random_position(self, grid):
+        """Set a random position to the item inside the grid"""
+        possible_position = []
+        for index_raw, _ in enumerate(grid):
+            for index_col, _ in enumerate(grid[index_raw]):
+                case_value = grid[index_raw][index_col]
+                if case_value == '0':
+                    possible_position.append([index_raw, index_col])
+
+        position_object = random.choice(possible_position)
+        self.pos_x = position_object[1]
+        self.pos_y = position_object[0]
+
     def draw_objet(self, image):
+        """draw the image in the screen"""
         sprite(image, self.pos_x * 25, self.pos_y * 25)
 
-
 class Heros:
-
-    OBJECTCOUNT = 0
+    """The hero class with all his function"""
+    object_count = 0
 
     def __init__(self):
+        """ we create the hero with the position of his start"""
         self.pos_x = 0
         self.pos_y = 2
 
     def draw_hero(self, image):
+        """will dislay the hero on the screen "* 25"
+        allow us to display the image at the right position """
         sprite(image, self.pos_x * 25, self.pos_y * 25)
 
-    def take_object(self, objet):
-        self.OBJECTCOUNT += 1
-        print("Objet collécté :" + str(self.OBJECTCOUNT))
+    def take_object(self, item):
+        """ Hero can take an item and it will change his position on the screen"""
+        if self.pos_x == item.pos_x and self.pos_y == item.pos_y \
+        and item.taken is False:
+            item.pos_x = 1000 * 25
+            item.pos_y = 1000 * 25
+            item.taken = True
+            self.object_count += 1
+# class Badguy:
 
-class Badguy:
+#     def __init__(self):
+#         self.pos_x = 16
+#         self.pos_y = 10
 
-    def __init__(self):
-        self.pos_x = 16
-        self.pos_y = 10
+#     def draw_bad_guy(self, image):
+#         sprite(image, self.pos_x * 25, self.pos_y * 25)
 
-    def draw_bad_guy(self, image):
-        sprite(image, self.pos_x * 25, self.pos_y * 25)
-
-def random_position(grid):
-    possible_position = []
-    for index_raw, raw in enumerate(grid):
-        for index_col, col in enumerate(grid[index_raw]):
-            case_value = grid[index_raw][index_col]
-            if case_value == '0':
-                possible_position.append([index_raw, index_col])
-    return possible_position
 
 def sprite(wall_image, pos_x, pos_y):
     """display the right sprite in the right position
@@ -58,158 +72,137 @@ def sprite(wall_image, pos_x, pos_y):
     constant.GAME_DISPLAY.blit(wall_image, (pos_x, pos_y))
 
 def text(msg, color):
+    """display the Item section at the bottom of the screen"""
+
     screen_text = pygame.font.SysFont('Arial', 25).render(msg, True, color)
     constant.GAME_DISPLAY.blit(screen_text, (1 * 25, 17 * 25))
 
 def end_game(msg, color):
+    """Will display the right message wether the player won or lose"""
     constant.GAME_DISPLAY.fill((0, 0, 0))
     screen_text = pygame.font.SysFont('Arial', 25).render(msg, True, color)
     constant.GAME_DISPLAY.blit(screen_text, (425/2, 425/2))
     pygame.display.update()
 
-macGyver = Heros()
-guard = Badguy()
-seringe = Objet()
-needle = Objet()
-ether = Objet()
-plastic = Objet()
-position_object_1 = random.choice(random_position(constant.MAZE))
-seringe.pos_x = position_object_1[1]
-seringe.pos_y = position_object_1[0]
+guard_pos_x = 16
+guard_pos_y = 10
+MacGyver = Heros()
+# Guard = Badguy()
+Seringe = Item()
+Needle = Item()
+Ether = Item()
+Plastic = Item()
 
-position_object_2 = random.choice(random_position(constant.MAZE))
-needle.pos_x = position_object_2[1]
-needle.pos_y = position_object_2[0]
+Seringe.random_position(constant.MAZE)
+Needle.random_position(constant.MAZE)
+Ether.random_position(constant.MAZE)
+Plastic.random_position(constant.MAZE)
 
-position_object_3 = random.choice(random_position(constant.MAZE))
-ether.pos_x = position_object_3[1]
-ether.pos_y = position_object_3[0]
-
-position_object_4 = random.choice(random_position(constant.MAZE))
-plastic.pos_x = position_object_4[1]
-plastic.pos_y = position_object_4[0]
 def labyrinth():
+    """Set the structure of the labyrinth from the grid in the constant file,
+    each character will match a structure '0' is the path the player can move """
 
-    seringe_img = pygame.image.load(os.path.join('ressource', 'seringue.png'))
-    needle_img = pygame.image.load(os.path.join('ressource', 'aiguille.png'))
-    plastic_tube_img = pygame.image.load(os.path.join('ressource', 'tube_plastique.png'))
-    ether_img = pygame.image.load(os.path.join('ressource', 'ether.png'))
-    mc_gyver_img = pygame.image.load(os.path.join('ressource', 'MacGyver.png'))
-    guard_img = pygame.image.load(os.path.join('ressource', 'Gardien.png'))
-
-    top_left_corner = pygame.image.load(os.path.join('ressource', 'TopLeftCorner.png'))
-    top_right_corner = pygame.image.load(os.path.join('ressource', 'TopRightCorner.png'))
-    top_middle = pygame.image.load(os.path.join('ressource', 'MiddleTop.png'))
-    bot_right_corner = pygame.image.load(os.path.join('ressource', 'BottomRigthCorner.png'))
-    bot_left_corner = pygame.image.load(os.path.join('ressource', 'BottomLeftCorner.png'))
-    bot_middle = pygame.image.load(os.path.join('ressource', 'MiddleBot.png'))
-    left_middle = pygame.image.load(os.path.join('ressource', 'MiddleLeft.png'))
-    right_middle = pygame.image.load(os.path.join('ressource', 'MiddleRight.png'))
-    maze_wall = pygame.image.load(os.path.join('ressource', 'wall.png'))
+    structure = {
+        'seringe_img': pygame.image.load(os.path.join('ressource', 'seringue.png')),
+        'needle_img': pygame.image.load(os.path.join('ressource', 'aiguille.png')),
+        'plastic_tube_img': pygame.image.load(os.path.join('ressource', 'tube_plastique.png')),
+        'ether_img': pygame.image.load(os.path.join('ressource', 'ether.png')),
+        'mc_gyver_img': pygame.image.load(os.path.join('ressource', 'MacGyver.png')),
+        'guard_img': pygame.image.load(os.path.join('ressource', 'Gardien.png')),
+        'top_left_corner': pygame.image.load(os.path.join('ressource', 'TopLeftCorner.png')),
+        'top_right_corner': pygame.image.load(os.path.join('ressource', 'TopRightCorner.png')),
+        'top_middle': pygame.image.load(os.path.join('ressource', 'MiddleTop.png')),
+        'bot_right_corner': pygame.image.load(os.path.join('ressource', 'BottomRigthCorner.png')),
+        'bot_left_corner': pygame.image.load(os.path.join('ressource', 'BottomLeftCorner.png')),
+        'bot_middle': pygame.image.load(os.path.join('ressource', 'MiddleBot.png')),
+        'left_middle': pygame.image.load(os.path.join('ressource', 'MiddleLeft.png')),
+        'right_middle': pygame.image.load(os.path.join('ressource', 'MiddleRight.png')),
+        'maze_wall': pygame.image.load(os.path.join('ressource', 'wall.png'))}
 
     constant.GAME_DISPLAY.fill((0, 0, 0))
-    for index_raw, raw in enumerate(constant.MAZE):
-        for index_col, col in enumerate(constant.MAZE[index_raw]):
+    for index_raw, _ in enumerate(constant.MAZE):
+        for index_col, _ in enumerate(constant.MAZE[index_raw]):
             case_value = constant.MAZE[index_raw][index_col]
             screen_raw = index_col * 25
             screen_col = index_raw * 25
             if case_value == '1':
-                sprite(top_middle, screen_raw, screen_col)
+                sprite(structure['top_middle'], screen_raw, screen_col)
 
             elif case_value == '2':
-                sprite(top_left_corner, screen_raw, screen_col)
+                sprite(structure['top_left_corner'], screen_raw, screen_col)
 
             elif case_value == '3':
-                sprite(bot_left_corner, screen_raw, screen_col)
-     
+                sprite(structure['bot_left_corner'], screen_raw, screen_col)
+
             elif case_value == '4':
-                sprite(bot_right_corner, screen_raw, screen_col)
+                sprite(structure['bot_right_corner'], screen_raw, screen_col)
 
             elif case_value == '6':
-                sprite(left_middle, screen_raw, screen_col)
+                sprite(structure['left_middle'], screen_raw, screen_col)
 
             elif case_value == '5':
-                sprite(top_right_corner, screen_raw, screen_col)
+                sprite(structure['top_right_corner'], screen_raw, screen_col)
 
             elif case_value == '7':
-                sprite(right_middle, screen_raw, screen_col)
+                sprite(structure['right_middle'], screen_raw, screen_col)
 
             elif case_value == 'X':
-                sprite(bot_middle, screen_raw, screen_col)
+                sprite(structure['bot_middle'], screen_raw, screen_col)
 
             elif case_value == 'M':
-                sprite(maze_wall, screen_raw, screen_col)
+                sprite(structure['maze_wall'], screen_raw, screen_col)
 
-    seringe.draw_objet(seringe_img)
-    needle.draw_objet(needle_img)
-    plastic.draw_objet(plastic_tube_img)
-    ether.draw_objet(ether_img)
+    Seringe.draw_objet(structure['seringe_img'])
+    Needle.draw_objet(structure['needle_img'])
+    Plastic.draw_objet(structure['plastic_tube_img'])
+    Ether.draw_objet(structure['ether_img'])
 
-    macGyver.draw_hero(mc_gyver_img)
-    guard.draw_bad_guy(guard_img)
+    MacGyver.draw_hero(structure['mc_gyver_img'])
+    sprite(structure['guard_img'], guard_pos_x * 25, guard_pos_y * 25)
 
-    text("ITEM:", constant.YELLOW)
+    text("ITEM: {}".format(MacGyver.object_count), constant.YELLOW)
     # pygame.display.update()
 
-gameOn = True
-while gameOn:
-    pygame.init()
-    labyrinth()
+def game():
+    """Here is the game function, it regroups the labyrinth,
+    the Hero class method and set the player's move"""
+    game_on = True
+    while game_on:
+        pygame.init()
+        labyrinth()
 
-   
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_on = False
+            control = pygame.key.get_pressed()
+
+            if control[pygame.K_LEFT] and constant.MAZE[MacGyver.pos_y][MacGyver.pos_x - 1] == '0':
+                MacGyver.pos_x -= 1
+            if control[pygame.K_RIGHT] and constant.MAZE[MacGyver.pos_y][MacGyver.pos_x + 1] == '0':
+                MacGyver.pos_x += 1
+            if control[pygame.K_UP] and constant.MAZE[MacGyver.pos_y - 1][MacGyver.pos_x] == '0':
+                MacGyver.pos_y -= 1
+            if control[pygame.K_DOWN] and constant.MAZE[MacGyver.pos_y + 1][MacGyver.pos_x] == '0':
+                MacGyver.pos_y += 1
+
+            MacGyver.take_object(Plastic)
+            MacGyver.take_object(Ether)
+            MacGyver.take_object(Needle)
+            MacGyver.take_object(Seringe)
+
+            if MacGyver.pos_x == guard_pos_x and MacGyver.pos_y == guard_pos_y \
+            and MacGyver.object_count >= 3:
+                game_on = False
+                end_game("GAGNÉ", constant.GREEN)
+                time.sleep(4)
+
+            elif MacGyver.pos_x == guard_pos_x and MacGyver.pos_y == guard_pos_y \
+            and MacGyver.object_count < 3:
+                game_on = False
+                end_game("PERDU", constant.RED)
+                time.sleep(4)
+
     pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameOn = False
-        control = pygame.key.get_pressed()
 
-        if control[pygame.K_LEFT] and constant.MAZE[macGyver.pos_y][macGyver.pos_x - 1] == '0' :
-            macGyver.pos_x -= 1
-        if control[pygame.K_RIGHT] and constant.MAZE[macGyver.pos_y][macGyver.pos_x + 1] == '0':
-            macGyver.pos_x += 1
-        if control[pygame.K_UP] and constant.MAZE[macGyver.pos_y - 1][macGyver.pos_x] == '0':
-            macGyver.pos_y -= 1
-        if control[pygame.K_DOWN] and constant.MAZE[macGyver.pos_y + 1][macGyver.pos_x] == '0':
-            macGyver.pos_y += 1
-
-        if macGyver.pos_x == plastic.pos_x and macGyver.pos_y == plastic.pos_y and plastic.TAKEN == False:
-            macGyver.take_object(plastic)
-            plastic.pos_x = 4 * 25
-            plastic.pos_y = 17 * 25
-            plastic.TAKEN = True
-
-        if macGyver.pos_x == seringe.pos_x and macGyver.pos_y == seringe.pos_y and seringe.TAKEN == False:
-            macGyver.take_object(seringe)
-            seringe.pos_x = 6 * 25
-            seringe.pos_y = 17 * 25
-            seringe.TAKEN = True
-        
-        if macGyver.pos_x == ether.pos_x and macGyver.pos_y == ether.pos_y and ether.TAKEN == False:
-            macGyver.take_object(ether)
-            ether.pos_x = 8 * 25
-            ether.pos_y = 17 * 25
-            ether.TAKEN = True
-            
-        if macGyver.pos_x == needle.pos_x and macGyver.pos_y == needle.pos_y and needle.TAKEN == False:
-            macGyver.take_object(needle)
-            needle.pos_x = 10 * 25
-            needle.pos_y = 17 * 25
-            needle.TAKEN = True
-        
-        if macGyver.pos_x == guard.pos_x and guard.pos_y == guard.pos_y and macGyver.OBJECTCOUNT >= 3:
-            gameOn = False
-            end_game("GAGNÉ", constant.GREEN)
-            time.sleep(4)
-
-        elif macGyver.pos_x == guard.pos_x and guard.pos_y == guard.pos_y and macGyver.OBJECTCOUNT < 3:
-            gameOn = False
-            end_game("PERDU", RED)
-            time.sleep(4)
-
-        
-        
-pygame.display.update()
-    
-            
-            
-
+game()
